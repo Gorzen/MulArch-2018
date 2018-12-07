a6c19a3607962119a23c6291a27c4f2663954f4b
Loris Pilotto <loris.pilotto@epfl.ch>
2018-12-07 20:31:46 +0100

    loris 1ere version

--- Assignement4/implementation.cu
+++ Assignement4/implementation.cu
@@ -46,42 +46,38 @@ void array_process(double *input, double *output, int length, int iterations)
     }
 }
 
-__global__
-void compute_gpu(double* gpu_input, double* gpu_output, double* gpu_temp, int length){
-    int x_glob = (blockIdx.x * blockDim.x) + threadIdx.x;
-    int y_glob = (blockIdx.y * blockDim.y) + threadIdx.y;
 
-    if(!(x_glob >= length-1 || x_glob <= 0 || y_glob >= length-1 || y_glob <= 0)){
-    	gpu_output[(x_glob)*(length)+(y_glob)] = (gpu_input[(x_glob-1)*(length)+(y_glob-1)] +
-                                                  gpu_input[(x_glob-1)*(length)+(y_glob)]   +
-             	                                  gpu_input[(x_glob-1)*(length)+(y_glob+1)] +
-                                                  gpu_input[(x_glob)*(length)+(y_glob-1)]   +
-                     	                          gpu_input[(x_glob)*(length)+(y_glob)]     +
-                                                  gpu_input[(x_glob)*(length)+(y_glob+1)]   +
-                                                  gpu_input[(x_glob+1)*(length)+(y_glob-1)] +
-                                                  gpu_input[(x_glob+1)*(length)+(y_glob)]   +
-                                                  gpu_input[(x_glob+1)*(length)+(y_glob+1)] ) / 9;
+__global__
+void compute_GPU(double* gpu_input, double* gpu_output, double* gpu_temp, int gpu_length){
+    int x_glob = (blockIdx.x * blockDim.x) + threadIdx.x ;
+    int y_glob = (blockIdx.y * blockDim.y) + threadIdx.y ;
+	
+	if(!(x_glob >= gpu_length-1 || x_glob <= 0 || y_glob >= gpu_length-1 || y_glob <= 0)){
+    	gpu_output[(y_glob)*(gpu_length)+(x_glob)] = (gpu_input[(x_glob-1)*(gpu_length)+(y_glob-1)] +
+                                                  gpu_input[(x_glob-1)*(gpu_length)+(y_glob)]   +
+             	                                  gpu_input[(x_glob-1)*(gpu_length)+(y_glob+1)] +
+                                                  gpu_input[(x_glob)*(gpu_length)+(y_glob-1)]   +
+                     	                          gpu_input[(x_glob)*(gpu_length)+(y_glob)]     +
+                                                  gpu_input[(x_glob)*(gpu_length)+(y_glob+1)]   +
+                                                  gpu_input[(x_glob+1)*(gpu_length)+(y_glob-1)] +
+                                                  gpu_input[(x_glob+1)*(gpu_length)+(y_glob)]   +
+                                                  gpu_input[(x_glob+1)*(gpu_length)+(y_glob+1)] ) / 9;
     }
-
+	
     __syncthreads();
-}
-
-__global__
-void invert_temp_gpu(double* gpu_input, double* gpu_output, double* gpu_temp, int length){
-    if(blockIdx.x == 0 && blockIdx.y == 0 && threadIdx.x == 0 && threadIdx.y){
-    	gpu_output[(length/2-1)*length+(length/2-1)] = 1000;
-    	gpu_output[(length/2)*length+(length/2-1)]   = 1000;
-    	gpu_output[(length/2-1)*length+(length/2)]   = 1000;
-    	gpu_output[(length/2)*length+(length/2)]     = 1000;
+	
+	if(blockIdx.x == 0 && blockIdx.y == 0 && threadIdx.x == 0 && threadIdx.y){
+    	gpu_output[(gpu_length/2-1)*gpu_length+(gpu_length/2-1)] = 1000;
+    	gpu_output[(gpu_length/2)*gpu_length+(gpu_length/2-1)]   = 1000;
+    	gpu_output[(gpu_length/2-1)*gpu_length+(gpu_length/2)]   = 1000;
+    	gpu_output[(gpu_length/2)*gpu_length+(gpu_length/2)]     = 1000;
 
     	gpu_temp = gpu_input;
     	gpu_input = gpu_output;
     	gpu_output = gpu_temp;
     }
-
     __syncthreads();
 }
-
 
 // GPU Optimized function
 void GPU_array_process(double *input, double *output, int length, int iterations)
@@ -96,55 +92,50 @@ void GPU_array_process(double *input, double *output, int length, int iterations
     cudaEventCreate(&comp_end);
 
     /* Preprocessing goes here */
-    size_t SIZE = length * length;
-    double* gpu_input;
-    double* gpu_output;
-    double* gpu_temp;
-
-    cudaMalloc((void**) &gpu_input, SIZE);
-    cudaMalloc((void**) &gpu_output, SIZE);
-    cudaMalloc((void**) &gpu_temp, SIZE);
-    /* End preprocessing       */
+	double* GPU_input;
+    double* GPU_output;
+	double* GPU_temp;
+	int* GPU_length;
+	size_t size_n = length * length * sizeof(double)
+	cudaMalloc((void**) &GPU_input, size_n);
+    cudaMalloc((void**) &GPU_output, size_n);
+	cudaMalloc((void**) &GPU_temp, size_n);
+	cudaMalloc((void**) &GPU_length, sizeof(int));
 
     cudaEventRecord(cpy_H2D_start);
     /* Copying array from host to device goes here */
-    cudaMemcpy((void*)gpu_input,
-	       (void*)input,
-	       SIZE,
-	       cudaMemcpyHostToDevice);
-    /* End copy array				   */
+	cudaMemcpy((void*) GPU_input, (void*) &input, size_n, cudaMemcpyHostToDevice);
+	cudaMemcpy((void*) GPU_output, (void*) &output, size_n, cudaMemcpyHostToDevice);
+	cudaMemcpy((void*) GPU_length, (void*) length, sizeof(int), cudaMemcpyHostToDevice);
+
     cudaEventRecord(cpy_H2D_end);
     cudaEventSynchronize(cpy_H2D_end);
 
     //Copy array from host to device
     cudaEventRecord(comp_start);
     /* GPU calculation goes here */
-    dim3 thrsPerBlock(length, length);
-    dim3 nBlks(1,1);
-
-    for(int n = 0; n < iterations; n++){
-	compute_gpu <<< nBlks, thrsPerBlock >>> (gpu_input, gpu_output, gpu_temp, length);
-
-	invert_temp_gpu <<< nBlks, thrsPerBlock >>> (gpu_input, gpu_output, gpu_temp, length);
-    }
-    /* End GPU calculation	 */
+	dim3 nBlks(1,1);
+	dim3 thrsPerBlock(length, length);
+	for(int n = 0; n < iterations; n++){
+	    compute_GPU <<< nBlks, thrsPerBlock >>> (GPU_input, GPU_output, GPU_temp, GPU_length);
+	}
+	
     cudaEventRecord(comp_end);
     cudaEventSynchronize(comp_end);
 
     cudaEventRecord(cpy_D2H_start);
     /* Copying array from device to host goes here */
-    cudaMemcpy((void*)output,
-	       (void*)gpu_output,
-	       SIZE,
-	       cudaMemcpyDeviceToHost);
-    /* End copy array 				   */
+	cudaMemcpy((void*) &input, (void*) GPU_input, size_n, cudaMemcpyDeviceToHost);
+	cudaMemcpy((void*) &output, (void*) GPU_output, size_n, cudaMemcpyDeviceToHost);
+
     cudaEventRecord(cpy_D2H_end);
     cudaEventSynchronize(cpy_D2H_end);
 
     /* Postprocessing goes here */
-    cudaFree((void**) &gpu_input);
-    cudaFree((void**) &gpu_output);
-    cudaFree((void**) &gpu_temp);
+	cudaFree((void**) &GPU_input);
+    cudaFree((void**) &GPU_output);
+    cudaFree((void**) &GPU_temp);
+	cudaFree((void*) &GPU_length);
 
     float time;
     cudaEventElapsedTime(&time, cpy_H2D_start, cpy_H2D_end);
